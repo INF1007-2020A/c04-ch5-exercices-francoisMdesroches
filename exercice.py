162465f409a0a0c372de3f231bebbe2393d63411
Francois <f.deroche@hotmail.com>
2020-09-22 19:53:26 -0400

    j'ai failli oublier de commit oups

--- exercice.py
+++ exercice.py
@@ -6,29 +6,90 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0 :
+        number = number * -1
+    return number
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
+    nouvTab = []
+    for i in prefixes:
+        nouvTab.append(i + suffixe)
+    return nouvTab
 
-    return [""]
+def est_premier(nombre : int) -> bool:
+    for i in range(2, nombre//2):
+        if nombre % i == 0:
+            return False
 
+    return True
 
 def prime_integer_summation() -> int:
-    return 0
+    nb_premiers = 3
+    somme_premiers = 10
+    i = 6
+
+    while nb_premiers < 100:
+        if est_premier(i):
+            somme_premiers += i
+            nb_premiers += 1
+        i += 1
+
+    # En python, il est possible de faire sum(tableau), ce qui additionne tous ses éléments
+    return somme_premiers
 
 
 def factorial(number: int) -> int:
-    return 0
+    nouvNombre = 1
+    for i in range(1, number+1):
+        nouvNombre = nouvNombre * i
+    return nouvNombre
 
 
 def use_continue() -> None:
+    for i in range(1, 10):
+        if i == 5:
+            continue
+        print(i)
     pass
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+
+    liste_fin = []
+
+    for groupe in groups :
+        if len(groupe) > 10 or len(groupe) <= 3:
+            liste_fin.append(False)
+            continue
+        if 25 in groupe:
+            liste_fin.append(True)
+            continue
+
+        plus_de_70 = False
+        exactement_50 = False
+        a_mineur = False
+
+        if 50 in groupe:
+            exactement_50 = True
+
+        # Au lieu d'une autre boucle, il aurait été possible d'utiliser "max" et "min".
+        # Wow les tableaux en Python. Enfin, les listes.
+
+        for membre in groupe:
+            if membre < 18:
+                a_mineur = True
+                break
+            elif membre > 70:
+                plus_de_70 = True
+
+        if a_mineur or (plus_de_70 and exactement_50):
+            liste_fin.append(False)
+        else:
+            liste_fin.append(True)
+
+    return liste_fin
 
 
 def main() -> None:
